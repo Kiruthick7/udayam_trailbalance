6722f21e4e86096ab0715eafd702ba70620c17f8
Kiruthick B <kiruthick@Kiruthicks-MacBook-Air.local>
2026-03-09 11:04:58 +0530

    build prod

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+echo "Installing Flutter dependencies..."
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter --version
+
+echo "Running flutter pub get"
+flutter pub get
+
+echo "Installing CocoaPods"
+cd ios
+pod install
+cd ..
